f2405b4bd5f709c5c8c798c4fe3593c337ba0125
Peter Grzybowski <peter@northern.tech>
2023-01-10 11:41:50 +0100

    test: monitor client tests: sort emails by device date.
    Ticket: QA-510
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- tests/tests/test_monitor_client.py
+++ tests/tests/test_monitor_client.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Northern.tech AS
+# Copyright 2023 Northern.tech AS
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 #    limitations under the License.
 #
 
+import datetime
 import json
 import os
 import os.path
@@ -106,19 +107,42 @@ def get_and_parse_email(env, address):
         # concat and create header object for each
         headers = []
         message_string = ""
+        device_date = None
         for line in mail.splitlines():
             if line.startswith(message_mail_options_prefix):
                 continue
             if message_start == line:
                 message_string = ""
+                device_date = None
                 continue
             if message_end == line:
-                headers.append(Parser(policy=default).parsestr(message_string))
+                if device_date is not None:
+                    logger.info("got device_date msg:%s {{{" % message_string)
+                    logger.info(device_date)
+                    message_string = (
+                        device_date.strftime("Date: %a, %m %b %Y %H:%M:%S +0200")
+                        + "\n"
+                        + message_string
+                    )
+                    logger.info("got device_date msg:%s }}}" % message_string)
+                h = Parser(policy=default).parsestr(message_string)
+                # if device_date:
+                #     h["Date"] = device_date.timestamp()
+                headers.append(h)
                 continue
             # extra safety, we are supposed to only eval b'string' lines
             if not line.startswith("b'"):
                 continue
-            message_string = message_string + eval(line).decode("utf-8") + "\n"
+            line_string = eval(line).decode("utf-8")
+            message_string = message_string + line_string + "\n"
+            # get the date from b'Time on device: Thu, 01 Dec 2022 14:01:01 UTC' line
+            if line_string.startswith("Time on device: "):
+                device_date = datetime.datetime.strptime(
+                    line_string, "Time on device: %a, %m %b %Y %H:%M:%S %Z"
+                )
+                logger.info("parsed device_date {{{")
+                logger.info(device_date)
+                logger.info("parsed device_date }}}")
 
     # log all messages for debug
     for m in headers:
@@ -924,6 +948,8 @@ class TestMonitorClientEnterprise:
         # sort the emails by subject to avoid wrong-order issues because of delivery time
         if isK8S():
             messages.sort(key=lambda x: x["Subject"])
+        else:
+            messages.sort(key=lambda x: x["Date"])
 
         assert_valid_alert(
             messages[0],
